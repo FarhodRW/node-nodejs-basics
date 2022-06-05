0d83aabef9ca99998fee9a2c08441e30ced9890a
FarhodRW <farhodrailway@gmail.com>
2022-06-05 09:05:34 +0500

    solved cp, fs, hash, modules, streams

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const name1 = 'name1 = "value1"';
+  const name2 = 'name2 = "value2"';
+
+  console.log(`RSS_` + name1);
+  console.log(`RSS_` + name2);
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,8 @@
 export const parseEnv = () => {
   // Write your code here
+  const name1 = 'name1 = "value1"';
+  const name2 = 'name2 = "value2"';
+
+  console.log(`RSS_` + name1);
+  console.log(`RSS_` + name2);
 };
